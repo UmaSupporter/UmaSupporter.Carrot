ff8fa98c0bdfe16bdebcd055869ee53480c1488e
PINOT <pinot.kim@kakao.com>
2022-04-14 01:48:47 +0900

    정보 업데이트 API 완성

---
+++ pages/api/refresh/[uma_id]/card.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import getConfig from "next/config";
+import axios from "axios";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { uma_id } = req.query;
+  const { API_PASSWORD } = getConfig().serverRuntimeConfig.CONFIG;
+
+  try {
+    await axios.post(`https://suppoter.sonagi.dev/ops/new/card`, {
+      root_password: API_PASSWORD,
+      new_card_uri: `https://gamewith.jp/uma-musume/article/show/${uma_id}`,
+    });
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+
+  return res.status(200).end();
+}
