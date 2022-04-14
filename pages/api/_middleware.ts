1543fb86ef7a33acfb74a0603a2adebe542b0f84
PINOT <pinot.kim@kakao.com>
2022-04-14 23:28:57 +0900

    Add middleware to verify token

---
+++ pages/api/_middleware.ts
@@ -0,0 +1,30 @@
+import { NextRequest } from "next/server";
+import jwt, { JsonWebTokenError } from "jsonwebtoken";
+
+export async function middleware(req: NextRequest) {
+  const adminPw = process.env.ADMIN_PW || "";
+  const authorization = req.headers.get("Authorization");
+  if (authorization) {
+    const token = authorization.split(" ")[1];
+    try {
+      jwt.verify(token, adminPw);
+    } catch (e) {
+      console.error(e);
+      if (e instanceof JsonWebTokenError) {
+        return new Response(e.message, {
+          status: 401,
+          headers: {
+            "Set-Cookie": "token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT",
+          },
+        });
+      } else {
+        return new Response(null, {
+          status: 401,
+          headers: {
+            "Set-Cookie": "token=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT",
+          },
+        });
+      }
+    }
+  }
+}
