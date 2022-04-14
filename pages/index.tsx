3a1a588459fbf8047962e7799cc77d7a9561f880
PINOT <pinot.kim@kakao.com>
2022-04-14 15:23:12 +0900

    Update index page text

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div className={"container mx-auto px-4 py-8"}>
-      <h1 className={"text-3xl font-black"}>HELLO WORLD!</h1>
-      <p className={"text-xl"}>This is just template project.</p>
+      <h1 className={"text-3xl font-black"}>UmaSupporter.Carrot</h1>
+      <p className={"text-xl"}>🏃🏽‍♀️ 우마무스메 육성 도우미인 &apos;우마서포터&apos;의 오퍼레이팅 툴입니다.</p>
     </div>
   );
 };
