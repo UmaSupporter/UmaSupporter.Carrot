d095c594275934571c55e9184176133ee65a4144
PINOT <pinot.kim@kakao.com>
2022-04-15 04:39:18 +0900

    Add `prop-alert` class for prop className

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -95,7 +95,7 @@ export function Alert({ close, type, title, message }: IAlertProps) {
       initial={{ opacity: 0, y: 50, scale: 0.3 }}
       animate={{ opacity: 1, y: 0, scale: 1 }}
       exit={{ opacity: 0, scale: 0.5, transition: { duration: 0.2 } }}
-      className={`alert alert-${type} shadow-lg`}
+      className={`alert prop-alert-${type} shadow-lg`}
     >
       <div>
         <Icon type={type} />
