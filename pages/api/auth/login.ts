3ebcf5be14bc6648949e600ed96b25355a74f955
PINOT <pinot.kim@kakao.com>
2022-04-14 22:59:36 +0900

    Add encryption on password

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import getConfig from "next/config";
+import { sha512 } from "js-sha512";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -9,7 +10,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { id, password } = req.body;
 
     if (!id || !password) return res.status(400).end();
-    if (id !== adminId || password !== adminPw) return res.status(401).end();
+    if (id !== adminId || sha512(password) !== sha512(adminPw)) return res.status(401).end();
 
     return res.status(200).end();
   } catch (e) {
