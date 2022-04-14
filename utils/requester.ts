193733fbdf2e933c5626c106d42c2a49072dafad
PINOT <pinot.kim@kakao.com>
2022-04-14 23:39:11 +0900

    Do authorization when token is set

--- utils/requester.ts
+++ utils/requester.ts
@@ -1,10 +1,15 @@
 import axios from "axios";
 import getConfig from "next/config";
+import { getCookie } from "cookies-next";
 
 const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
+const token = getCookie("token");
 
 const instance = axios.create({
   baseURL: `${publicRuntimeConfig.CONFIG.API_HOST ?? serverRuntimeConfig.CONFIG.API_HOST}/api`,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : ``,
+  },
 });
 
 export default instance;
