48c51486ae5d4818802f245e2cc0bd164bd2b6e7
PINOT <pinot.kim@kakao.com>
2022-04-14 23:10:16 +0900

    Add `token` cookie for keep session

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -1,6 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import getConfig from "next/config";
 import { sha512 } from "js-sha512";
+import jwt from "jsonwebtoken";
+import { setCookies } from "cookies-next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -11,6 +13,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     if (!id || !password) return res.status(400).end();
     if (id !== adminId || sha512(password) !== sha512(adminPw)) return res.status(401).end();
+
+    const token = jwt.sign({ id, password: sha512(password) }, adminPw, { expiresIn: "7d" });
+    setCookies("token", token, {
+      req,
+      res,
+      maxAge: 60 * 60 * 24 * 7,
+    });
 
     return res.status(200).end();
   } catch (e) {
